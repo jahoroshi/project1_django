8dd2b5ef09069f92381fa92438d078eb5803ff0b
jahoroshi4y <jahoroshi4y@gmail.com>
2024-09-04 13:45:06 +0300

    Updated deployment configs.

--- project1/deployment_configs/check_and_run.sh
+++ project1/deployment_configs/check_and_run.sh
@@ -8,4 +8,4 @@ if lsof -i :$PORT > /dev/null; then
     sleep 2
 fi
 
-exec /home/ubuntu/.cache/pypoetry/virtualenvs/project1-6Bpdu8kL-py3.12/bin/gunicorn --workers 3 --bind 0.0.0.0:8000 -c /home/ubuntu/project1/project1/config/gunicorn.conf.py project_1.wsgi:application
+exec /home/ubuntu/.cache/pypoetry/virtualenvs/project1-6Bpdu8kL-py3.12/bin/gunicorn --workers 3 --bind 0.0.0.0:8000 -c /home/ubuntu/project1/project1/deployment_configs/gunicorn.conf.py project_1.wsgi:application
