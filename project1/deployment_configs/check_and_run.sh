11ea6328e0e9c2e1c097739b6d15ad506cdeaabe
jahoroshi4y <jahoroshi4y@gmail.com>
2024-09-04 19:31:08 +0300

    Updated deployment configs.

--- project1/deployment_configs/check_and_run.sh
+++ project1/deployment_configs/check_and_run.sh
@@ -3,7 +3,7 @@
 PORT=8000
 
 if lsof -i :$PORT > /dev/null; then
-    echo "Port $PORT is already in use. Killing all processes using the port."
+    echo "Port $PORT is already in use. Killing all processes using the port"
     lsof -t -i :$PORT | xargs -r kill -9
     sleep 2
 fi
